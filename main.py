5e957c755df5a4789bbaa0b4debe2fd60d0a9af5
asbdu386cb22s615273 <144540846+asbdu386cb22s615273@users.noreply.github.com>
2023-09-10 20:04:16 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-def Hello_World():
-print("Hello world")
-Hello_world()
+#Imlementation a recurcive calculate a 
+factorial of a given number
+def recur_factorial(n):
+  if n==1:
+    reurn n
+  else:
+    return n*recur_factorial(n-1)
+#take input from the user
+num=int(input(Enter a number:))    
+#check is the number is negative
+if num<0:
+  print("sorr,factorial does not exist for negative numbers")
+elif num==0:
+  print("The factorial of 0 is 1")he factorial of",num,"is",recure_factorial(num))
